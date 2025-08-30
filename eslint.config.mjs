f009a44c738ad78d792cf6ad3bbe604fe9be49dc
deomiarn <deomicarnel@hotmail.com>
2025-08-30 15:13:57 +0200

    feat: add ESLint configuration and TypeScript support with necessary dependencies

---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+// eslint.config.mjs
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: [
+      '**/node_modules/**',
+      '**/dist/**',
+      '**/.next/**',
+      '**/coverage/**'
+    ],
+  },
+  {
+    files: ['**/*.ts', '**/*.tsx'],
+    languageOptions: {
+      parser: tseslint.parser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+    },
+    plugins: {
+      '@typescript-eslint': tseslint.plugin,
+    },
+    extends: [
+      js.configs.recommended,
+      ...tseslint.configs.recommended, // non type-checked preset
+    ],
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+  }
+);
